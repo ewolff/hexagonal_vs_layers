809783d001d79b8c1de23222b60c7be420b34836
Eberhard Wolff <eberhard.wolff@posteo.net>
2026-07-30 07:38:41 +0200

    Add UI layer

--- hexagonal_vs_layers/src/hexagonal_vs_layers/LayerTest.java
+++ hexagonal_vs_layers/src/hexagonal_vs_layers/LayerTest.java
@@ -6,13 +6,21 @@ import org.junit.jupiter.api.Test;
 
 import hexagonal_vs_layers.database.MyRepository;
 import hexagonal_vs_layers.logic.MyBusinessLogicClass;
+import hexagonal_vs_layers.ui.MyUI;
 
 class LayerTest {
 
 	@Test
-	void test() {
+	void testBusinesssLogic() {
 		MyBusinessLogicClass businessLogic = new MyBusinessLogicClass(new MyRepository());
 		businessLogic.someBusinessLogic();
+	}
+
+	@Test
+	void testUI() {
+		MyBusinessLogicClass businessLogic = new MyBusinessLogicClass(new MyRepository());
+		MyUI ui = new MyUI(businessLogic);
+		ui.userDidSomething();
 	}
 
 }
