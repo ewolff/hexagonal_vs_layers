2b0472554fdfd6bb1ad3e3423c1e373b0e696e09
Eberhard Wolff <eberhard.wolff@posteo.net>
2026-08-02 22:14:33 +0200

    Establish database adapter

--- hexagonal/src/main/java/com/ewolff/hexagonal_vs_layers/database_adapater/MyRepository.java
+++ hexagonal/src/main/java/com/ewolff/hexagonal_vs_layers/database_adapater/MyRepository.java
@@ -1,4 +1,4 @@
-package com.ewolff.hexagonal_vs_layers.database;
+package com.ewolff.hexagonal_vs_layers.database_adapater;
 
 import com.ewolff.hexagonal_vs_layers.logic.database_port.Repository;
 
